5d87156871bc9c2743194c72e936034785b100fc
jiangjiali <jiali.jiang@unspay.com>
2018-05-16 10:38:57 +0800

    333

--- src/test/java/com/homePage/ObtainPlateParametersTest.java
+++ src/test/java/com/homePage/ObtainPlateParametersTest.java
@@ -15,7 +15,7 @@ import com.example.HttpUtil;
 
 public class ObtainPlateParametersTest extends HttpUtil {
 // 获取板块参数接口
-	String url = "uu-admin/container/getContainerParam";
+	String url = "/uu-admin/container/getContainerParam";
 
 
 	/**
@@ -23,219 +23,158 @@ public class ObtainPlateParametersTest extends HttpUtil {
 	 */
 	@Test
 	public void postObtainPlateParametersTestCorrectParameter() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", "12495324");
-				
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
-		
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", "12495324");
+						
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("提交正确参数" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(0);
-		assertThat(head1.get("msg")).isEqualTo("成功");
+		assertThat(post.get("status")).isEqualTo(0);
+		assertThat(post.get("msg")).isEqualTo("成功");
 	}
 	/**
 	 * 用户ID为未登录用户
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdNotLoggedIn() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", "12495324123");		
-		
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
-		
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", "12495324123");		
+			
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为未登录用户" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID为错误用户
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdIsError() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", "12312sd313");
-				
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);		
-		
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", "12312313");
+						
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为错误用户" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
-	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID为非法字符
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdIllegalCharacters() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", "<$%^>");									
-			    		
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
-				
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", "<$%^>");									
+			    						
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为非法字符" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID为小数
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdIsDecimal() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", 121123.33);		
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", 121123.33);		
 		
-		
-		
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
-		
-		
+			
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为小数" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
-	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID为负数
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdIsNegativeNumber() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", -121312);
-		
-		
-		
-		
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
-		
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", -121312);
 		
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为负数" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID为空格
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdIsSpace() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", " ");		
-		
-				
-		
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", " ");		
 		
 		
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为空格" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID为空
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdIsEmpty() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", "");		
-		
-		
-		
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", "");		
 		
 		
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为空" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID为null
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdIsNull() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", null);		
-		
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", null);		
 				
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
-		
 		
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为null" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID为0
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdIsZero() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		con.put("userId", 0);
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("userId", 0);
 	
-		
-		
-		
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
-		
-		
+	
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID为0" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	/**
 	 * 用户ID不传该参数
 	 */
 	//@Test
 	public void postObtainPlateParametersTestUserIdNonSubmissionParameters() throws Exception {
-		Map<String, Object> con = new HashMap<String, Object>();
-		
-		
-		
-		Map<String, Object> request = new HashMap<String, Object>(); // 给request赋值
-		request.put("con", con);
-		
+		Map<String, Object> request = new HashMap<String, Object>();	
 		
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("用户ID不传该参数" + post);
-		JSONObject head1 = (JSONObject) post.get("head");
 	
-		assertThat(head1.get("st")).isEqualTo(-3);
-		assertThat(head1.get("msg")).isEqualTo("数据包错误！");
+		assertThat(post.get("st")).isEqualTo(-3);
+		assertThat(post.get("msg")).isEqualTo("数据包错误！");
 	}
 	
 	
