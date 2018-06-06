bebcb3f4a7a65a3b789f32941b10e7d6c41fa438
钱君超 <13774323645@163.com>
2018-06-06 18:14:24 +0800

    111

--- src/test/java/com/userManagement/UnLockedUserTest.java
+++ src/test/java/com/userManagement/UnLockedUserTest.java
@@ -8,29 +8,31 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 //import org.junit.Test;
 
-public class GetManagerParaSpecialTest extends HttpUtil {
+public class UnLockedUserTest extends HttpUtil {
 // 搜索获取日志列表接口
-	String url = "/uu-admin/BackUser/getUserOperateLog";
+	String url = "/uu-admin/UUuserManage/unLockedUser";
 	String userId;
 	@BeforeClass
 	public void beforeClass(){
 		userId = new BackUserLoginTest().userId;
 	}
-
 	/**
 	 * 提交正确参数
 	 */
 	@Test
-	public void postGetManagerParaSpecialTestCorrectParameter() throws Exception {
+	public void postGetUserOperateLogTestCorrectParameter() throws Exception {
 		Map<String, Object> request = new HashMap<String, Object>();
-		request.put("userId", 10);
-		request.put("ParaId", 1);
+		request.put("userId", userId);
+		request.put("lockedUserId", 1);
+		request.put("lockId", 0);
+
 		
 		JSONObject post = super.UNSPost(url, request);
 		System.out.println("提交正确参数" + post);
